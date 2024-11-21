8fdcd182f12c5fde322ceb577bc80d6cf38f959b
yanggy <ilike.yanggy@gmail.com>
2024-11-21 17:03:55 +0800

    事件发布

--- my-springread-start/src/main/java/com/readspring/pubsub/PubSubMain.java
+++ my-springread-start/src/main/java/com/readspring/pubsub/PubSubMain.java
@@ -9,8 +9,8 @@ import com.readspring.pubsub.v3.TObjectEventV3;
 import org.springframework.context.annotation.*;
 
 /**
- * spring 事件发布器解决泛型擦除问题，事件发布器发布带泛型的事件 event， 监听器如何监听到带有泛型的事件 event。泛型都是object，
- * 即如何自动泛型的具体类型是哪个？
+ * spring 事件发布器解决泛型擦除问题，事件发布器发布带泛型的事件 event， 监听器如何监听到带有泛型的事件 event。泛型都是object，?
+ * 即如何知道泛型的具体类型是哪个，从而找到该具体类型的监听器？
  * 核心逻辑就在 ResolvableTypeProvider 接口里面，重写了 getResolvableType 方法，
  * 在运行期动态的获取泛型对应的真正的对象类型，从而解决了编译阶段泛型擦除的问题。
  */
